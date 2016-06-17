7d9f9c7d0a62408740ca52295e85b113e47713e7
301203 <301203@W8074203.adsh5173.com>
2016-06-17 23:39:39 +0800

    first commit

---
+++ src/main/java/spring/seiya/chapter2/helloworld/HelloApi.java
@@ -0,0 +1,5 @@
+package spring.seiya.chapter2.helloworld;
+
+public interface HelloApi {
+	public void sayHello();
+}
---
+++ src/main/java/spring/seiya/chapter2/helloworld/HelloImpl.java
@@ -0,0 +1,8 @@
+package spring.seiya.chapter2.helloworld;
+
+public class HelloImpl implements HelloApi {
+	
+	public void sayHello(){
+		System.out.println("Hello World!");
+	}
+}
