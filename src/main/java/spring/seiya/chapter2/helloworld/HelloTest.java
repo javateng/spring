a62d63687c667d2454c7229b14c8283b1f7c5b78
seiya <seiya@DESKTOP-AHC2V3S>
2016-06-20 19:26:22 +0800

    xx

--- src/main/java/spring/seiya/chapter2/helloworld/HelloTest.java
+++ src/main/java/spring/seiya/chapter2/helloworld/HelloTest.java
@@ -10,7 +10,14 @@ public class HelloTest {
 		//1、读取配置文件实例化一个IoC容器
 		ApplicationContext context = new ClassPathXmlApplicationContext("helloworld.xml");
 		//2、从容器中获取Bean，注意此处完全“面向接口编程，而不是面向实现”
-		HelloApi helloApi = context.getBean("hello",HelloApi.class);
+		//方法1：
+		//HelloApi helloApi = context.getBean("hello",HelloApi.class);
+		//方法2：
+		HelloApi helloApi = context.getBean(HelloApi.class);
+		//方法3：
+		//HelloApi helloApi = (HelloImpl) context.getBean("hello");
+		
+		
 		//3、执行业务逻辑
 		helloApi.sayHello();
 	}
